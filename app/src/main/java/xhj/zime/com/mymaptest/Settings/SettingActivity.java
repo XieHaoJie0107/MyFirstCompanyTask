5227eabb5ad954a7e2b9ca5a513fbd381f5c6c5e
谢浩杰 <1012351395@qq.com>
2018-08-17 09:28:39 +0800

    增加了关于我们界面,其实是一个百度的WebView,设置客户端,不跳转到自带浏览器

--- app/src/main/java/xhj/zime/com/mymaptest/Settings/SettingActivity.java
+++ app/src/main/java/xhj/zime/com/mymaptest/Settings/SettingActivity.java
@@ -13,25 +13,43 @@ import xhj.zime.com.mymaptest.ActivityCollector.BaseActivity;
 import xhj.zime.com.mymaptest.Main.MainActivity;
 import xhj.zime.com.mymaptest.R;
 
-public class SettingActivity extends BaseActivity {
+public class SettingActivity extends BaseActivity implements View.OnClickListener {
+    private LinearLayout mAbout, mFinishAll;
+    private ImageButton mBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-        ImageButton seetting_img_back_to_main = (ImageButton) findViewById(R.id.setting_back_to_main);
-        seetting_img_back_to_main.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-        LinearLayout linearLayout5 = (LinearLayout) findViewById(R.id.linearLayout5);
-        linearLayout5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        initView();
+        mBack.setOnClickListener(this);
+        mFinishAll.setOnClickListener(this);
+        mAbout.setOnClickListener(this);
+    }
+
+    private void initView() {
+        mBack = (ImageButton) findViewById(R.id.setting_back_to_main);
+        mFinishAll = (LinearLayout) findViewById(R.id.linearLayout5);
+        mAbout = (LinearLayout) findViewById(R.id.linearLayout3);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.linearLayout5:
                 ActivityCollector.finishAll();
-            }
-        });
+                break;
+            case R.id.setting_back_to_main:
+                finish();
+                break;
+            case R.id.linearLayout3:
+                openAboutUs();
+                break;
+        }
+    }
+
+    private void openAboutUs() {
+        Intent intent = new Intent(this,SettingAboutUsActivity.class);
+        startActivity(intent);
     }
 }
